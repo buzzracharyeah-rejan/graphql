7ccf17fb297a4bdc3e9118ea3b1a06805f034b52
rejan-buzzracharyeah <rejandev@gmail.com>
2022-07-22 14:16:08 +0545

    make cleanup to setup express-graphql server

--- src/app.js
+++ src/app.js
@@ -1,27 +1,13 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-const { ApolloServer } = require('apollo-server');
 
 const connectDB = require('./config/_db');
-const { gql } = require('apollo-server-core');
-const { books } = require('./sample-data');
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
 require('dotenv').config({ path: path.join(__dirname, '/config/.env') });
 
 // register app globally
 global.app = express();
 app.use(morgan('dev'));
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  csrfPrevention: true,
-  cache: 'bounded',
-});
-
-server.listen().then(({ url }) => {
-  connectDB();
-  console.log(`🚀  Server ready at ${url}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server started at port ${PORT}`));
